c5d6e1907d0e44354eee3440cf609bbaa7ba399e
gonzagustin10 <gonzalososa2003@gmail.com>
2024-10-17 14:50:48 -0300

    Resuelvo conflictos después de git pull

--- src/app.client/src/components/CartPage/Checkout.jsx
+++ src/app.client/src/components/CartPage/Checkout.jsx
@@ -6,7 +6,7 @@ export function Checkout({ products }) {
   const [total, setTotal] = useState(0);
   const [isInitialized, setIsInitialized] = useState(false);
 
-<<<<<<< HEAD
+
   useEffect(() => {
     // Llamada al servidor PHP para obtener el preference_id
     fetch('http://localhost/TUP---Proyecto/src/app.server/index.php', {
@@ -92,76 +92,3 @@ export function Checkout({ products }) {
     </div>
   );
 }
-=======
-export function Checkout ({products}) {
-    return(
-    <>
-        <div className="Checkout-Conteiner" >
-            <div className="Checkout-Box">
-                    <h3>Envio</h3>
-                <hr />
-                <form>
-                    <div className="Checkout-inputTextBox">
-                        <div className="Checkout-inputTextSubBox">
-                            <input type="input" className="Checkout-inputText" placeholder="postalCode" name="postalCode" id='postalCode' required />
-                            <label htmlFor="name" className="Checkout-inputTextLabel">Codigo postal</label>
-                        </div>
-                    </div>
-                    <div className='Checkout-inputRadio'>
-                        <input type="radio" id="OcaDomicilio" name="methodShipping" value="OcaDomicilio"></input>
-                        <label htmlFor="OcaDomicilio">OCA Domicilio ${}</label>
-                    </div>
-                    <div className='Checkout-inputRadio'>
-                        <input type="radio" id="OcaSucursal" name="methodShipping" value="OcaSucursal"></input>
-                        <label htmlFor="OcaSucursal">OCA Sucursal ${}</label>
-                    </div>
-                    <div className='Checkout-inputRadio'>
-                        <input type="radio" id="retiroAlmacen" name="methodShipping" value="retiroAlmacen" defaultChecked></input>
-                        <label htmlFor="retiroAlmacen">Retiro en almacen ${}</label>
-                    </div>
-                </form>
-            </div>
-            <div className="Checkout-Box">
-                <h3>Descuento</h3>
-                <hr />
-
-                <form>
-                    <div className="Checkout-inputTextBox">
-                        <div className="Checkout-inputTextSubBox">
-                            <input type="input" className="Checkout-inputText" placeholder="discountCode" name="discountCode" id='discountCode' required />
-                            <label htmlFor="name" className="Checkout-inputTextLabel">Codigo de descuento</label>
-                        </div>
-                    </div>
-                </form>
-
-            </div>
-            <div className="Checkout-Box">
-                <h3>Pago</h3>
-                <hr />
-                <form>
-                    <div className='Checkout-inputRadio'>
-                        <input type="radio" id="cash" name="methodPayment" value="cash" defaultChecked ></input>
-                        <label htmlFor="transfer">Efectivo</label>
-                    </div>
-                    <div className='Checkout-inputRadio'>
-                        <input type="radio" id="transfer" name="methodPayment" value="transfer"></input>
-                        <label htmlFor="transfer">Depósito o Transferencia Bancaria</label>
-                    </div>
-                    <div className='Checkout-inputRadio'>
-                        <input type="radio" id="debitCard" name="methodPayment" value="debitCard"></input>
-                        <label htmlFor="debitCard">Mercadopago - Tarjetas Online, PagoFacil, RapiPago</label>
-                    </div>
-                    <div className='Checkout-inputRadio'>
-                        <input type="radio" id="creditCard" name="methodPayment" value="creditCard"></input>
-                        <label htmlFor="creditCard">Tarjetas de crédito ¡Hasta 12 cuotas!</label>
-                    </div>
-                </form>
-            </div>
-            <div className="Checkout-Buttons">
-               <button>Completar compra</button>
-            </div>
-        </div>
-    </>
-    )
-}
->>>>>>> ad0e4db9cd31c1d79b574a5a857b31e758959d73
