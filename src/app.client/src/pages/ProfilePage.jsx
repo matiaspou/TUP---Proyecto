8a72106ded8a288a42188dc4d6db08d487a691e8
Matias Pou <matiaspou5@gmail.com>
2024-10-18 23:28:09 -0300

    se inicia github page

--- src/app.client/src/pages/ProfilePage.jsx
+++ src/app.client/src/pages/ProfilePage.jsx
@@ -15,29 +15,7 @@ function ProfilePage() {
         <article className="ProfilePage-Article">
           <div className="ProfilePage-Conteiner">
             <div className="ProfilePage-Header"></div>
-              <div className="ProfilePage-Box1">
-              <table className='ProfilePage-Table'>
-                <thead>
-                  <th>ID </th>
-                  <th>Fecha de realizacion</th>
-                  <th>Monto</th>
-                  <th>Estado Pago</th>
-                  <th>Estado Envio</th>
-                  <th>Acciones</th>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>dada</td>
-                    <td>dada</td>
-                    <td>dada</td>
-                    <td>dada</td>
-                    <td>dada</td>
-                    <td>dada</td>
-                  </tr>
-                  
-                </tbody>
-              </table>
-              </div>
+                
           </div>
           <Footer></Footer>
         </article>
